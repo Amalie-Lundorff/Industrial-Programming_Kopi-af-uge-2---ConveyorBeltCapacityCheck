511a97a74c2fc60a393f44d3a68765bcfc32ad59
Amalie-Lundorff <s235664@dtu.dk>
2025-11-19 16:52:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+{
+    //Laver vægt på motoren konstant
+    const decimal maxkgpermotor = 5.6m; 
+    
+    //Her udfylder brugeren data 
+    Console.WriteLine("How many motors are carrying packages?");
+    int.TryParse(Console.ReadLine(), out var motors);
+    
+    Console.WriteLine("How many kg of packages do we expect?");
+    decimal.TryParse(Console.ReadLine(), out var kg);
+    
+    //Vægt pr. motor 
+    var weightPerMotor = kg / motors;
+    
+    //Udskrivning af resultat
+    if (weightPerMotor <= maxkgpermotor)
+        Console.WriteLine("Yes! The Conveyor belt can carry the packages");
+    else 
+        Console.WriteLine("No! The Conveyor belt can carry the packages");
+    
+}
